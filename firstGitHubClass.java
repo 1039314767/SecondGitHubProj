5aa4005081597babb7ef5702c9b32b0f5b8e4ef3
1039314767 <64476745+1039314767@users.noreply.github.com>
2020-04-28 21:52:38 +0800

    add a row java code

--- firstGitHubClass.java
+++ firstGitHubClass.java
@@ -2,5 +2,6 @@
 public class firstGitHubClass{
 	public static void main(String[] args){
 		System.out.println("Hello World ! First learn GitHub !");
+		System.out.println("add a row code");		
 	}	
 }
